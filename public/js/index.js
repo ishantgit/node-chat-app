5584b906e4a9bb4ccea183098781286b88a5def4
ishantrana <ishant@atcouch.com>
2017-05-05 13:11:51 +0530

    scroll to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 var socket = io();
 
+function scrollToBottom(){
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+};
 socket.on('connect',function (){
   console.log('Connected to server');
 
@@ -22,6 +36,7 @@ socket.on('newMessage',function (message){
     createdAt: formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
   // console.log('new message',message);
   // var li = jQuery('<li></li>');
   // li.text(`${message.from} ${formattedTime}: ${message.text}`);
@@ -39,6 +54,7 @@ socket.on('newLocationMessage',function(message){
     createdAt: formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
   // var li = jQuery('<li></li>');
   // var a = jQuery('<a target="_blank">Current Location</a>')
   // li.text(`${message.from} ${formattedTime} `);
